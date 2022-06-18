98d2ea3555c074c9e168752adfe23e1c74289c72
Geovany <marlongeo1999@gmail.com>
2022-06-17 20:58:39 -0600

    📲 no mobile supported view, created

--- cliente/src/pages/LandingPage.js
+++ cliente/src/pages/LandingPage.js
@@ -3,12 +3,27 @@ import { Home } from '../componentes/Home'
 import { NuestroEquipo } from '../componentes/NuestroEquipo'
 import { QuienesSomos } from '../componentes/QuienesSomos'
 
+import '../styles/Home.css'
+
 export const LandingPage = () => {
   return (
     <div>
-      <Home/>
-      <QuienesSomos/>
-      <NuestroEquipo/>
+      <div className='mobile-only'>
+        <h1>
+          AUN NO TENEMOS SOPORTE PARA
+          <span style={{ color: '#7ac678' }}> VERSION MOBILE🤗</span>
+        </h1>
+        <h4>Por favor, utiliza la vista de Escritorio</h4>
+        <div>
+          Mientras nosotros trabajamos👷‍♂️👷‍♀️ para traerte la experiencia del CEI
+          en mobile 📲
+        </div>
+      </div>
+      <div className='desktop-only'>
+        <Home />
+        <QuienesSomos />
+        <NuestroEquipo />
+      </div>
     </div>
   )
 }
