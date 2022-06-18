c8cabf1105a29bb7c7fb618f5c4f918a01a1c53c
Geovany <marlongeo1999@gmail.com>
2022-06-18 12:03:10 -0600

    🐜 fixed responsive navbar

--- cliente/src/componentes/Navbar.js
+++ cliente/src/componentes/Navbar.js
@@ -6,53 +6,69 @@ export const Navbar = () => {
   const [isOpen, setIsOpen] = useState('')
 
   return (
-    <div className='navbar'>
-      <div className='navbar-links'>
-        <Link
-          to='/'
-          className={isOpen === 'home' ? 'navbar__link dot' : 'navbar__link'}
-          onClick={() => setIsOpen('home')}
-        >
-          Home
-        </Link>
-        <Link
-          to='/calendario'
-          className={isOpen === 'cal' ? 'navbar__link dot' : 'navbar__link'}
-          onClick={() => setIsOpen('cal')}
-        >
-          Calendario
-        </Link>
-        <Link
-          to='/casos-de-exito'
-          className={isOpen === 'casos' ? 'navbar__link dot' : 'navbar__link'}
-          onClick={() => setIsOpen('casos')}
-        >
-          Casos de exito
-        </Link>
-        <Link
-          to='/blog'
-          className={isOpen === 'blog' ? 'navbar__link dot' : 'navbar__link'}
-          onClick={() => setIsOpen('blog')}
-        >
-          Blog
-        </Link>
+    <>
+      <div className='navbar'>
+        <div className='navbar-links'>
+          <div>
+            <Link
+              to='/'
+              className={
+                isOpen === 'home' ? 'navbar__link dot' : 'navbar__link'
+              }
+              onClick={() => setIsOpen('home')}
+            >
+              Home
+            </Link>
+          </div>
+          <div>
+            <Link
+              to='/calendario'
+              className={isOpen === 'cal' ? 'navbar__link dot' : 'navbar__link'}
+              onClick={() => setIsOpen('cal')}
+            >
+              Calendario
+            </Link>
+          </div>
+          <div>
+            <Link
+              to='/casos-de-exito'
+              className={
+                isOpen === 'casos' ? 'navbar__link dot' : 'navbar__link'
+              }
+              onClick={() => setIsOpen('casos')}
+            >
+              Casos de exito
+            </Link>
+          </div>
+          <div>
+            <Link
+              to='/blog'
+              className={
+                isOpen === 'blog' ? 'navbar__link dot' : 'navbar__link'
+              }
+              onClick={() => setIsOpen('blog')}
+            >
+              Blog
+            </Link>
+          </div>
+        </div>
+        <div className='navbar-btns'>
+          <Link
+            to='/contactanos'
+            className='btn__contactanos'
+            onClick={() => setIsOpen('contact')}
+          >
+            Contactanos
+          </Link>
+          <Link
+            to='/postulate'
+            className='btn__postulate'
+            onClick={() => setIsOpen('postulate')}
+          >
+            Postulate
+          </Link>
+        </div>
       </div>
-      <div className='navbar-btns'>
-        <Link
-          to='/contactanos'
-          className='btn__contactanos'
-          onClick={() => setIsOpen('contact')}
-        >
-          Contactanos
-        </Link>
-        <Link
-          to='/postulate'
-          className='btn__postulate'
-          onClick={() => setIsOpen('postulate')}
-        >
-          Postulate
-        </Link>
-      </div>
-    </div>
+    </>
   )
 }
