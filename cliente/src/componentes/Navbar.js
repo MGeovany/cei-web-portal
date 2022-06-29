36e65be75bd3f5e4d78bdbce86ec7b4386270d45
Geovany <marlongeo1999@gmail.com>
2022-06-28 22:11:21 -0600

    🐊 all navbar built

--- cliente/src/componentes/Navbar.js
+++ cliente/src/componentes/Navbar.js
@@ -5,6 +5,7 @@ import React, { useState } from 'react'
 export const Navbar = () => {
   const [active, setActive] = useState('nav__menu')
   const [icon, setIcon] = useState('nav__toggler')
+  const [isOpen, setIsOpen] = useState('')
 
   const navToggle = () => {
     if (active === 'nav__menu') {
@@ -18,25 +19,60 @@ export const Navbar = () => {
   return (
     <nav className='nav'>
       <ul className={active}>
+        <li className='nav__item'></li>
         <li className='nav__item'>
-          <Link to='/' className='nav__link'>
+          <Link
+            to='/'
+            className={isOpen === 'home' ? 'nav__link dot' : 'nav__link'}
+            onClick={() => setIsOpen('home')}
+          >
             Home
           </Link>
         </li>
         <li className='nav__item'>
-          <Link to='/calendario' className='nav__link'>
+          <Link
+            to='/calendario'
+            className={isOpen === 'cal' ? 'nav__link dot' : 'nav__link'}
+            onClick={() => setIsOpen('cal')}
+          >
             Actividades
           </Link>
         </li>
         <li className='nav__item'>
-          <Link to='/casos-de-exito' className='nav__link'>
+          <Link
+            to='/casos-de-exito'
+            className={isOpen === 'casos' ? 'nav__link dot' : 'nav__link'}
+            onClick={() => setIsOpen('casos')}
+          >
             Casos de Éxito
           </Link>
         </li>
         <li className='nav__item'>
-          <Link to='/blog' className='nav__link'>
+          <Link
+            to='/blog'
+            className={isOpen === 'blog' ? 'nav__link dot' : 'nav__link'}
+            onClick={() => setIsOpen('blog')}
+          >
             blog
           </Link>
+        </li>
+        <li>
+          <div className='navbar-btns'>
+            <Link
+              to='/contactanos'
+              className='btn__contactanos'
+              onClick={() => setIsOpen('')}
+            >
+              Contáctanos
+            </Link>
+            <Link
+              to='/postulate'
+              className='btn__postulate'
+              onClick={() => setIsOpen('')}
+            >
+              Postulate
+            </Link>
+          </div>
         </li>
       </ul>
       <div onClick={navToggle} className={icon}>
