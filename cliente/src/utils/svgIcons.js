d67653e669043d56a752069001573d3348afa307
Geovany <marlongeo1999@gmail.com>
2022-07-19 20:38:47 -0600

    🦸 hero banner added

--- cliente/src/utils/svgIcons.js
+++ cliente/src/utils/svgIcons.js
@@ -1,19 +1,21 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 
 // home svg
 export const Arrow = (props) => (
   <svg
-    width={24}
-    height={24}
-    fill='#7ac678'
-    transform='rotate(180)'
     xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 330 330'
+    width={props.width}
+    height={props.height}
+    style={{
+      enableBackground: 'new 0 0 330 330'
+    }}
+    xmlSpace='preserve'
+    fill='#ffffff'
     {...props}
   >
-    <path
-      d='M20.329 11v2H7.5l3.243 3.243-1.414 1.414L3.672 12l5.657-5.657 1.414 1.414L7.5 11H20.33Z'
-      fill='#e1575f'
-    />
+    <path d='M15 180h263.787l-49.394 49.394c-5.858 5.857-5.858 15.355 0 21.213C232.322 253.535 236.161 255 240 255s7.678-1.465 10.606-4.394l75-75c5.858-5.857 5.858-15.355 0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213 0-5.858 5.857-5.858 15.355 0 21.213L278.787 150H15c-8.284 0-15 6.716-15 15s6.716 15 15 15z' />
   </svg>
 )
 
@@ -73,5 +75,23 @@ export const HomeRed = (props) => (
       d='M0 572.741s395.353 67.374 588.3-12.716 210.169-140.439 448.2-42.005 378.051-44.806 616.083 28 267.777 26.718 267.777 26.718v-122.8L0 419Z'
       fill='#e35961'
     />
+  </svg>
+)
+
+export const HeroBanner = (props) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={'100%'}
+    height={'100%'}
+    fill='#ffffff'
+    {...props}
+  >
+    <g data-name='Grupo 5' fill='#e35961'>
+      <path data-name='Rect\xE1ngulo 182' d='M.359 0h1920v343H.359z' />
+      <path
+        data-name='Trazado 7'
+        d='M0 469.314s395.353 70.69 588.3-13.342 210.169-147.351 448.2-44.073 378.051-47.011 616.083 29.382 267.777 28.033 267.777 28.033v-128.84L0 308.007Z'
+      />
+    </g>
   </svg>
 )
