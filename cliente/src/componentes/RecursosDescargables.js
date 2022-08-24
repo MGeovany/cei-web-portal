c1fb960d135b5d45259d06031c81132e7fb7cde1
Edwin Geul <edwingeul01@gmail.com>
2022-08-23 19:34:38 -0600

    Responsive 'Resources'  - Fixed up

--- cliente/src/componentes/RecursosDescargables.js
+++ cliente/src/componentes/RecursosDescargables.js
@@ -47,7 +47,22 @@ export const RecursosDescargables = () => {
     slidesToShow: 4,
     slidesToScroll: 4,
     arrows: true,
-    vertical: false
+    vertical: false,
+    responsive:[
+      {breakpoint: 690,
+      settings: {slidesToShow: 1,  slidesToScroll: 1 }
+
+      },
+      {breakpoint: 1050,
+        settings: {slidesToShow: 2,  slidesToScroll: 2 }
+  
+      },
+      {breakpoint: 1450,
+        settings: {slidesToShow:3,  slidesToScroll: 3 }
+  
+      }
+      
+    ]
   }
 
   return (
