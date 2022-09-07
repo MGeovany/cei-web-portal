57bb227d680f68439bf383c5fbdb8093075cd858
Geovany <marlongeo1999@gmail.com>
2022-09-06 19:45:35 -0600

    added modal to home

--- cliente/src/componentes/HomeAdmin.js
+++ cliente/src/componentes/HomeAdmin.js
@@ -1,31 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Arrow, HeroBanner } from '../utils/svgIcons'
+import { Group, Modal } from '@mantine/core'
+import { HomeForm } from './HomeForm'
 
 export const HomeAdmin = () => {
-  return (
-    <div className='home'>
-      <div className='home__banner'>
-        <HeroBanner />
-      </div>
-      <div className='home__text'>
-        <h1 className='home__title'>
-          Bienvenido al panel administrativo del CEI
-        </h1>
-        <div className='home__btns'>
-          <div className='home__btn__propuesta'>Editar Contenido</div>
+  const [opened, setOpened] = useState(false)
 
-          <Link to='/'>
-            <div className='home__btn__blog'>
-              Ver Vista Principal
-              <Arrow fill='#ffffff' width={24} height={23} />
+  return (
+    <>
+      <div className='home'>
+        <div className='home__banner'>
+          <HeroBanner />
+        </div>
+        <div className='home__text'>
+          <h1 className='home__title'>
+            Bienvenido al panel administrativo del CEI
+          </h1>
+          <div className='home__btns'>
+            <div
+              className='home__btn__propuesta'
+              onClick={() => setOpened(true)}
+            >
+              Editar Contenido
             </div>
-          </Link>
+
+            <Link to='/'>
+              <div className='home__btn__blog'>
+                Ver Vista Principal
+                <Arrow fill='#ffffff' width={24} height={23} />
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className='home__img'>
+          <img src='/assets/people/Startup.png' alt='Startup' />
         </div>
       </div>
-      <div className='home__img'>
-        <img src='/assets/people/Startup.png' alt='Startup' />
-      </div>
-    </div>
+
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        overlayOpacity={0.55}
+        centered={true}
+        padding={'xl'}
+        size={'1080px'}
+        overlayBlur={3}
+      >
+        <Group position='left'>
+          <div className='asset__modal__title'>
+            Editar Contenido de{' '}
+            <span style={{ color: '#e1575f' }}>Pagina Inicio</span>
+          </div>
+        </Group>
+        <HomeForm />
+      </Modal>
+    </>
   )
 }
---
+++ cliente/src/componentes/HomeForm.js
@@ -0,0 +1,165 @@
+import { Button, Grid, Input, Textarea } from '@mantine/core'
+import { useForm } from '@mantine/form'
+import React from 'react'
+import { useState, useRef } from 'react'
+import { FileButton, Group, Text } from '@mantine/core'
+export const HomeForm = () => {
+  const [file, setFile] = useState(null)
+  const resetRef = useRef(null)
+
+  const clearFile = () => {
+    setFile(null)
+    resetRef.current?.()
+  }
+
+  const form = useForm({
+    initialValues: {
+      email: '',
+      termsOfService: false
+    },
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Correo Invalido')
+    }
+  })
+  return (
+    <div>
+      <form
+        className='form__caso__center'
+        onSubmit={form.onSubmit((values) => console.log(values))}
+      >
+        <Grid grow columns={24}>
+          <Grid.Col span={12}>
+            <Input.Wrapper
+              className='font-sen'
+              id='input-nombre-proyecto'
+              required
+              label='Título'
+            >
+              <Input
+                id='input-nombre-proyecto'
+                placeholder='Bienvenidos al CEI'
+              />
+            </Input.Wrapper>
+          </Grid.Col>
+          <Grid.Col span={12}>
+            <Textarea
+              minRows={2}
+              maxRows={15}
+              className='font-sen'
+              placeholder='Sitio Web del Centro de Emprendimiento e Innovación'
+              label='Subtitulo'
+              required
+            />
+          </Grid.Col>
+          <Grid.Col span={24}>
+            <Group position='left'>
+              <Text className='font-sen'>Imagen principal</Text>
+              <FileButton
+                resetRef={resetRef}
+                onChange={setFile}
+                accept='image/png,image/jpeg'
+                color='cyan'
+                radius='xl'
+                size='xs'
+              >
+                {(props) => <Button {...props}>Seleccionar imagen</Button>}
+              </FileButton>
+              <Button
+                disabled={!file}
+                onClick={clearFile}
+                color='red'
+                radius='xl'
+                size='xs'
+              >
+                Reiniciar
+              </Button>
+            </Group>
+            {file && (
+              <Text size='sm' align='left' mt='sm'>
+                Archivo seleccionado: {file.name}
+              </Text>
+            )}
+          </Grid.Col>
+          {/* 
+              CAROUSEL CONTENT
+          */}
+          <Grid.Col span={24}>
+            <Group position='left'>
+              <div className='asset__modal__title'>
+                Editar Contenido del{' '}
+                <span style={{ color: '#e1575f' }}>Carousel</span>
+              </div>
+            </Group>
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <Input.Wrapper
+              className='font-sen'
+              id='input-nombre-proyecto'
+              required
+              label='Título'
+            >
+              <Input
+                id='input-nombre-proyecto'
+                placeholder='Proyecto Manos de Vida'
+              />
+            </Input.Wrapper>
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <Input.Wrapper
+              className='font-sen'
+              id='input-nombre-proyecto'
+              required
+              label='Sección'
+            >
+              <Input id='input-nombre-proyecto' placeholder='Casos de exito' />
+            </Input.Wrapper>
+          </Grid.Col>
+          <Grid.Col span={24}>
+            <Textarea
+              minRows={2}
+              maxRows={15}
+              className='font-sen'
+              placeholder='Manos de Vida es una plataforma...'
+              label='Detalles'
+              required
+            />
+          </Grid.Col>
+          {/* 
+              QuienesSomos CONTENT
+          */}
+          {/* 
+              Integrantes CONTENT
+          */}
+          {/* 
+              Footer CONTENT
+          */}
+          <Grid.Col span={24}>
+            <Button
+              type='submit'
+              mt='sm'
+              sx={{ marginRight: '20px' }}
+              size='md'
+              style={{
+                boxShadow: '3px 3px 17px #00000029',
+                backgroundColor: '#072958'
+              }}
+            >
+              Enviar
+            </Button>
+            <Button
+              type='reset'
+              mt='sm'
+              size='md'
+              style={{
+                boxShadow: '3px 3px 17px #00000029',
+                backgroundColor: '#e1575f'
+              }}
+            >
+              Cancelar
+            </Button>
+          </Grid.Col>
+        </Grid>
+      </form>
+    </div>
+  )
+}
