1726c6b1ace759fc4fd4247bedf936587f7a42db
DITM5 <ditm1996@gmail.com>
2022-09-09 23:38:14 -0600

    boton logout

--- cliente/src/API/index.js
+++ cliente/src/API/index.js
@@ -12,3 +12,32 @@ export class Eventos {
         return result.json()
     }
 }
+
+export class Auth {
+    static validate = async function(usuario, contrasena){
+          let resultValidate = {'access':false, 'data':null};
+
+          return await fetch('https://cei1.herokuapp.com/1.0.0/login',{
+            method: "POST",
+            body: JSON.stringify({usuario: usuario, contrasena: contrasena}),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+          })
+          .then(response => response.json())
+          .then(json => {
+            if(json.usuario){
+                resultValidate.access = true;
+                resultValidate.data =  json
+                return resultValidate;
+            }else{
+                resultValidate.access = false;
+                resultValidate.data = 'Usuario o contraseña incorrecta';
+                return resultValidate;
+            }
+          })
+          .catch(err => {
+            resultValidate.access = false;
+            resultValidate.data = 'Ocurrio un error al validar el acceso';
+            return resultValidate;
+        });
+    }
+}
