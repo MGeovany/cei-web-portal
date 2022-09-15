df4d3e3ab9ec854b155fbdc4c3760aa71dc23829
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2022-09-15 01:39:26 -0600

    playwright moved to client

--- cliente/src/store/slices/login/loginSlice.js
+++ cliente/src/store/slices/login/loginSlice.js
@@ -2,72 +2,76 @@
 
 import { createSlice } from '@reduxjs/toolkit'
 
-var CryptoJS = require("crypto-js");
+import CryptoJS from 'crypto-js'
 const key = 'CEI2022'
 const maxMinutesSession = 15
 
 let dataSession = {
-  'expira': '',
-  'usuario': '',
-  'correo': '',
-  'nombre': ''
-};
+  expira: '',
+  usuario: '',
+  correo: '',
+  nombre: ''
+}
 
 export const getSession = () => {
-  let session = localStorage.getItem("SESSION");
-  if(session){
-    var bytes  = CryptoJS.AES.decrypt(session, key);
-    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  let session = localStorage.getItem('SESSION')
+  if (session) {
+    var bytes = CryptoJS.AES.decrypt(session, key)
+    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
   }
-  return null;
+  return null
 }
 
 const getMinutesSession = () => {
-  let session = getSession();
-  if(session){
-    var startSession = new Date(session.expira);
-    var now = new Date();
-    return (now.getTime() - startSession.getTime())/ 1000 / 60;
+  let session = getSession()
+  if (session) {
+    var startSession = new Date(session.expira)
+    var now = new Date()
+    return (now.getTime() - startSession.getTime()) / 1000 / 60
   }
-  return null;
+  return null
 }
 
 export const newSession = () => {
-  dataSession.expira = new Date().toLocaleString();
-  var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(dataSession), key).toString();
-  localStorage.setItem("SESSION", ciphertext);
-  watchSession();
+  dataSession.expira = new Date().toLocaleString()
+  var ciphertext = CryptoJS.AES.encrypt(
+    JSON.stringify(dataSession),
+    key
+  ).toString()
+  localStorage.setItem('SESSION', ciphertext)
+  watchSession()
 }
 
 export const removeSession = () => {
-  localStorage.removeItem("SESSION");
+  localStorage.removeItem('SESSION')
 }
 
 const watchSession = () => {
   setTimeout(() => {
-    if(!validateLogged()){
-      alertSession();
-    }else{
-      let session = getSession();
-      dataSession.usuario = session.usuario;
-      dataSession.correo = session.correo;
-      dataSession.nombre = session.nombre;
+    if (!validateLogged()) {
+      alertSession()
+    } else {
+      let session = getSession()
+      dataSession.usuario = session.usuario
+      dataSession.correo = session.correo
+      dataSession.nombre = session.nombre
     }
-  }, 1000);
+  }, 1000)
 }
 
 const alertSession = () => {
-  let modal = document.getElementById("alertSessionModalButton");
-  if(modal){
-    modal.click();
+  let modal = document.getElementById('alertSessionModalButton')
+  if (modal) {
+    modal.click()
   }
 }
 
 const validateLogged = () => {
-  let minutesSession = getMinutesSession();
-  const isLogged = (minutesSession > maxMinutesSession) || minutesSession == null ? false : true;
-  isLogged ? watchSession() : removeSession();
-  return isLogged;
+  let minutesSession = getMinutesSession()
+  const isLogged =
+    minutesSession > maxMinutesSession || minutesSession == null ? false : true
+  isLogged ? watchSession() : removeSession()
+  return isLogged
 }
 
 export const loginSlice = createSlice({
@@ -77,21 +81,21 @@ export const loginSlice = createSlice({
   },
   reducers: {
     login: (state, action) => {
-      dataSession.usuario = action.payload.usuario;
-      dataSession.correo = action.payload.correo;
-      dataSession.nombre = action.payload.nombre;
-      state.isLogged = true;
-      newSession();
+      dataSession.usuario = action.payload.usuario
+      dataSession.correo = action.payload.correo
+      dataSession.nombre = action.payload.nombre
+      state.isLogged = true
+      newSession()
     },
     logout: (state, action) => {
-      dataSession.usuario = '';
-      dataSession.correo = '';
-      dataSession.nombre = '';
-      state.isLogged = false;
-      removeSession();
+      dataSession.usuario = ''
+      dataSession.correo = ''
+      dataSession.nombre = ''
+      state.isLogged = false
+      removeSession()
     }
   }
-});
+})
 
 export const { login, logout, isAuth } = loginSlice.actions
 
