2b58ef49ad053ccb171c37fef0b1e956e49a6372
Geovany <marlongeo1999@gmail.com>
2022-09-15 16:08:53 -0600

    deleted unnecesary divs from landing

--- cliente/tests/example.spec.js
+++ cliente/tests/example.spec.js
@@ -1,21 +1,23 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test')
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('homepage has Playwright in title and get started link linking to the intro page', async ({
+  page
+}) => {
+  await page.goto('https://playwright.dev/')
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Playwright/)
 
   // create a locator
-  const getStarted = page.locator('text=Get Started');
+  const getStarted = page.locator('text=Get Started')
 
   // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+  await expect(getStarted).toHaveAttribute('href', '/docs/intro')
 
   // Click the get started link.
-  await getStarted.click();
-  
+  await getStarted.click()
+
   // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
+  await expect(page).toHaveURL(/.*intro/)
+})
