985cc545fbb44b61fa76b733a66bf0602de07687
Geovany <marlongeo1999@gmail.com>
2022-09-15 21:19:00 -0600

    added carousel test

--- cliente/tests/landing.spec.js
+++ cliente/tests/landing.spec.js
@@ -1,19 +1,30 @@
 // @ts-check
 
 const { test, expect } = require('@playwright/test')
+const { describe } = test
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page, browserName }) => {
   await page.goto('http://localhost:3000/')
+  test.skip(browserName === 'firefox', 'Still working on it')
+  test.skip(browserName === 'webkit', 'Still working on it')
 })
 
-test.describe('Landing Page should have six sections', () => {
+describe('Home Section', () => {
   test('Section Home, title is showing', async ({ page }) => {
     await expect(page.locator('.home__title')).toContainText(/\w/g)
   })
-  test('Section Home, subttitle is showing', async ({ page }) => {
+
+  test('Section Home , subttitle is showing', async ({ page }) => {
     await expect(page.locator('.home__subtitle')).toContainText(/\w/g)
   })
-  test('Section Home, image is valid', async ({ page }) => {
-    await expect(page.locator('img')).toHaveAttribute('alt', 'Startup')
+})
+
+describe('Carousel Section', () => {
+  test('Section Carousel, subttitle is showing', async ({ page }) => {
+    await expect(page.locator('.section-title')).toContainText([
+      /\w/g,
+      /\w/g,
+      /\w/g
+    ])
   })
 })
