4c096919e74b35e01ec5c32cd9a02a49e60f4de9
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2022-09-28 09:26:29 -0600

    added first test

--- cliente/cypress/e2e/app.cy.js
+++ cliente/cypress/e2e/app.cy.js
@@ -4,4 +4,29 @@ describe('All six sections can be visualized ', () => {
     cy.visit('http://localhost:3000/')
     cy.contains('AMAMOS AYUDARTE A CRECER')
   })
+
+  describe('App should do the main job', () => {
+    it('User should send a postulation', () => {
+      cy.visit('http://localhost:3000/')
+      cy.get('.home__btn__propuesta').click()
+      cy.get('#mantine-r1').type('Marlon Castro')
+      cy.get('#mantine-r2').type('marlon@castro')
+      cy.get('#mantine-r3').type('617111256')
+      cy.get('#mantine-r4').type('94836857')
+      cy.get('#mantine-r5').select('Masculino')
+      cy.get('#mantine-r6').select('Idea')
+      cy.get('#mantine-r7').type('CEI website')
+      cy.get('#mantine-r8').select('San Pedro Sula')
+      cy.get(
+        '.postulate-grid-item:nth-child(9) .mantine-Radio-radioWrapper:nth-child(1) > .mantine-qqmv3w'
+      ).click()
+      cy.get('#mantine-r9').click()
+      cy.get('#mantine-rb').click()
+      cy.get('#mantine-rf').click()
+      cy.get('#mantine-ri').click()
+      cy.get('.mantine-Group-root').click()
+      cy.get('.mantine-qo1k2').click()
+      cy.get('.mantine-qo1k2').click()
+    })
+  })
 })
