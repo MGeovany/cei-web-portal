16442d68f967f480bb6abdf3f7f630f38f02c11f
Diego Montoya <diegolink4@hotmail.com>
2022-10-06 19:42:35 -0600

    Endpoint de archivos descargables

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ app.set('json spaces',2);
 app.use(express.json());
 app.use(cors());
 
-app.use('/1.0.0',require('./src/1.0.0/routes/post')) 
-app.use('/1.0.0',require('./src/1.0.0/routes/Casos'))
+app.use('/1.0.0',require('./src/1.0.0/routes/post')) // Endpoint para el modulo de recursos&Blog
+app.use('/1.0.0',require('./src/1.0.0/routes/Casos'))// Endpoint para el modulo de casos
 app.use('/1.0.0',require('./src/1.0.0/routes/Usuario'))
 app.use('/1.0.0',require('./src/1.0.0/routes/Postulacion'))
 app.use('/1.0.0',require('./src/1.0.0/routes/imagenes'))
